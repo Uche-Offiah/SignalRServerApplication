66f0794598d66174c3644dec3bea32075a8dacc9
Uchechukwu Offiah <evaristusoffiah@gmail.com>
2024-05-27 23:09:19 +0100

    Initial model class

---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeodicBankAPI.Domain.Entities
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public byte[] Password { get; set; }
+        public string Email { get; set; }  
+        public string Phone { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public decimal Balance { get; set; }
+        public string City { get; set; }
+        public string? Country { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
