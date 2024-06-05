3f904591fcf0eda4af5ea1670acf5a192ba74677
Uchechukwu Offiah <evaristusoffiah@gmail.com>
2024-06-05 23:32:31 +0100

    Added Utility class

---
+++ Utility.cs
@@ -0,0 +1,17 @@
+﻿namespace SignalRChatApplication
+{
+    public static class Utility
+    {
+        public static void LogStuff(string stuff, string folderName)
+        {
+            var directory = $"C:/{folderName}/";
+            var fileName = DateTime.Now.Year.ToString() + "_" + DateTime.Now.Month.ToString() + "_" + DateTime.Now.DayOfYear.ToString() + "_" + "Log.txt";
+            if (!System.IO.Directory.Exists(directory))
+            {
+                System.IO.Directory.CreateDirectory(directory);
+            }
+
+            System.IO.File.AppendAllText(directory + fileName, Environment.NewLine + DateTime.Now.ToString() + Environment.NewLine + stuff + Environment.NewLine + Environment.NewLine);
+        }
+    }
+}
